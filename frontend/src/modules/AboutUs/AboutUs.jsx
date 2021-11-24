c20d4e56bcdd6fdf5b85512ad1748ba483ef4370
Schou <Jonasschou@live.dk>
2021-11-24 13:22:44 +0100

    About tekst

--- frontend/src/modules/AboutUs/AboutUs.jsx
+++ frontend/src/modules/AboutUs/AboutUs.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
 export const AboutUs = () => {
-  return <h1>About Us</h1>;
+  return (
+    <div style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            <p style={{ color: "#000000", fontSize: 26 }}>[img] Om os</p>
+            <p style={{ alignItems: "center",color: "black", fontSize: 16, fontFamily: "Arial, sans-serif", maxWidth: "80%" }}>
+                Foreningen ”Aarhus duck Race” er
+                en forening som ene og alene
+                på 100 % frivillig basis at afholde
+                dette løb og folkefest.
+			</p>
+            <p style={{ alignItems: "center",color: "black", fontSize: 16, fontFamily: "Arial, sans-serif", maxWidth: "80%" }}>
+                Samtidigt med at gøre det.
+                Har vi det skide sjovt. 
+                Det er jo altid sjovt at
+                gøre noget godt for andre.
+			</p>
+            <p style={{ alignItems: "center",color: "black", fontSize: 16, fontFamily: "Arial, sans-serif", maxWidth: "80%" }}>
+                Så har du lyst til at være frivillig.
+                Er du meget velkommen til at blive
+                medhjælper, og medlem af foreningen.
+			</p>
+        </div>
+  )
 };
