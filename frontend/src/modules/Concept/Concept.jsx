86abd9de819cdd2a32b46781f6e79919212435b4
Schou <Jonasschou@live.dk>
2021-11-24 12:57:50 +0100

    Tekst på Concept Screen

--- frontend/src/modules/Concept/Concept.jsx
+++ frontend/src/modules/Concept/Concept.jsx
@@ -1,5 +1,37 @@
 import React from "react";
 
 export const Concept = () => {
-  return <h1>Concept</h1>;
+  return(
+    <div style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            <p style={{ color: "#000000", fontSize: 26 }}>
+                Aarhus Duck Race
+            </p>
+            <p style={{ alignItems: "center",color: "black", fontSize: 16, fontFamily: "Arial, sans-serif", maxWidth: "80%" }}>
+                Aarhus Duckrace er et spændende festligt
+                familie arrangement. Hvor 5000 super
+                hurtige ænder dyster om at være den
+                hurtigste and på åen.
+			</p>
+            <p style={{ alignItems: "center",color: "black", fontSize: 16, fontFamily: "Arial, sans-serif", maxWidth: "80%" }}>
+                Hver and har et nummer, som du kan købe.
+                Vinder anden med dit nummer
+                er der super fede præmier til dig.
+			</p>
+            <p style={{ alignItems: "center",color: "black", fontSize: 16, fontFamily: "Arial, sans-serif", maxWidth: "80%" }}>
+                Samtidigt med løbet er årets<br/>
+                folkefest, med musik dans,
+                og andre sjove happenings i Mølleparken. 
+			</p>
+            <p style={{ alignItems: "center",color: "black", fontSize: 16, fontFamily: "Arial, sans-serif", maxWidth: "80%" }}>
+                Der vil være storskærm,
+                hvor du via en drone video
+                kan følge din and.
+                Og være helt tæt på løbet.
+			</p>
+            <p style={{ alignItems: "center",color: "black", fontSize: 16, fontFamily: "Arial, sans-serif", maxWidth: "80%" }}>
+                Vi glæder os til at se dig
+                den 1. Maj 2022 i Mølleparken.
+            </p>
+        </div>
+  )
 };
