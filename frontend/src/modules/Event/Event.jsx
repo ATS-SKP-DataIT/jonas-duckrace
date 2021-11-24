5f0cf5d0a8d5120ed5130f14f72657a00456101d
Schou <Jonasschou@live.dk>
2021-11-24 12:35:10 +0100

    "Hvor foregår det?" side tilføjelse

--- frontend/src/modules/Event/Event.jsx
+++ frontend/src/modules/Event/Event.jsx
@@ -1,5 +1,29 @@
 import React from "react";
 
 export const Event = () => {
-  return <h1>Event</h1>;
+  return (
+    <div style={{ flex: 1, textAlign: "center", alignItems: "center", justifyContent: "center" }}>
+      <p style={{ color: "#000000", fontSize: 30 }}>[img] Hvor foregår racet?</p>
+    <p style={{ alignItems: "center",color: "black", fontSize: 20, fontFamily: "Arial, sans-serif", maxWidth: "80%" }}>
+        [embed google map]
+</p>
+    <p style={{ alignItems: "center",color: "black", fontSize: 20, fontFamily: "Arial, sans-serif", maxWidth: "80%" }}>
+        Vore ænder skal tilbageligge den krævende
+        strækning fra Vester Alle til Immervad.
+</p>
+    <p style={{ alignItems: "center",color: "black", fontSize: 20, fontFamily: "Arial, sans-serif", maxWidth: "80%" }}>
+        Alt efter omstændighederne tager det
+        ænderne ca 45 minutter at tilbagelægge
+        strækningen. Her tages dog forbehold for
+        strøm, vind, drone, tang og
+        andre vanskeligheder.
+</p>
+    <p style={{ alignItems: "center",color: "black", fontSize: 20, fontFamily: "Arial, sans-serif", maxWidth: "80%" }}>
+        Samtidigt med dette vil der i Mølleparken
+        være en scene med live musik
+        og andre former for underholdning.
+        Så Rab Rab afsted!
+    </p>
+    </div>
+  )
 };
