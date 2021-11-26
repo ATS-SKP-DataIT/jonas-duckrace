cb789d49042bf145a8d6c1e3d899c71d8e12eabd
Schou <Jonasschou@live.dk>
2021-11-26 10:12:20 +0100

    Prized

--- frontend/src/modules/Prizes/Prizes.jsx
+++ frontend/src/modules/Prizes/Prizes.jsx
@@ -1,5 +1,40 @@
 import React from "react";
+import './prizes.css';
+
+// Bootstrap Import
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import Logo from "../../assets/img/page-header-duck.png";
+
 
 export const Prizes = () => {
-  return <h1>Prizes</h1>;
+  return(
+    <div style={{ flex: 1, textAlign: "center", alignItems: "center", justifyContent: "center" }}>
+      <p style={{ marginTop: 20, color: "#000000", fontSize: 25  }}> 
+        <img src={Logo} alt="Error"/> Præmier
+      </p>
+      <Container>
+        <Row>
+          <Col>1 of 3</Col>
+        </Row>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col>2 of 3</Col>
+          <Col>3 of 3</Col>
+        </Row>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col>2 of 3</Col>
+          <Col>3 of 3</Col>
+        </Row>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col>2 of 3</Col>
+          <Col>3 of 3</Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
