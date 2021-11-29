d1c13abc64547de49389497b9ffe8dd55395191d
Karsten Reitan Sørensen <krs@aarhustech.dk>
2021-11-29 08:33:38 +0100

    Altered url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,49 +16,21 @@ import { Donate } from "./modules/Donate/Donate"
 
 function App() {
     return ( <
-        BrowserRouter basename = { "/skp-projekt-aarhus-duck-race" } >
-        <
-        Layout >
-        <
-        Switch >
-        <
-        Route path = "/"
-        exact = { true }
-        component = { Home }
-        /> <
-        Route path = "/concept"
-        exact = { true }
-        component = { Concept }
-        /> <
-        Route path = "/buy"
-        exact = { true }
-        component = { Buy }
-        /> <
-        Route path = "/event"
-        exact = { true }
-        component = { Event }
-        /> <
-        Route path = "/prizes"
-        exact = { true }
-        component = { Prizes }
-        /> <
-        Route path = "/purpose"
-        exact = { true }
-        component = { Purpose }
-        /> <
-        Route path = "/AboutUs"
-        exact = { true }
-        component = { AboutUs }
-        /> <
-        Route path = "/Donate"
-        exact = { true }
-        component = { Donate }
-        /> <
-        Route component = { NotFound }
-        /> <
-        /Switch> <
-        /Layout> <
-        /BrowserRouter>
+    <BrowserRouter basename={"/skp-projekt-aarhus-duck-race"}>
+      <Layout>
+        <Switch>
+          <Route path="/" exact={true} component={Home} />
+          <Route path="/concept" exact={true} component={Concept} />
+          <Route path="/buy" exact={true} component={Buy} />
+          <Route path="/event" exact={true} component={Event} />
+          <Route path="/prizes" exact={true} component={Prizes} />
+          <Route path="/purpose" exact={true} component={Purpose} />
+          <Route path="/AboutUs" exact={true} component={AboutUs} />
+          <Route path="/Donate" exact={true} component={Donate} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
     );
 }
 
