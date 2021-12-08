7fbd22a41d0764d125b26978bda015ef7dc437a8
Alex <alexmoller02@hotmail.com>
2021-12-08 08:45:34 +0100

    Navbar er blevet mere responsiv

--- frontend/src/modules/Home/Home.jsx
+++ frontend/src/modules/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.style.css";
 import { LoadingSplash } from "../LoadingSplash/LoadingSplash";
 import { useSpring, animated } from "react-spring";
@@ -6,6 +6,10 @@ import { useSpring, animated } from "react-spring";
 import { Calendar } from "./Calendar";
 
 export const Home = () => {
+  const [visible, setVisible] = useState(0);
+
+
+  
   const animateSplash = useSpring({
     from: {
       opacity: 1,
@@ -26,14 +30,13 @@ export const Home = () => {
     },
     config: {duration: 4000}
   });
-
   
   return (
     <>
-      <animated.div style={animateSplash} className="splash">
-        <LoadingSplash id="hideMe"/>
-      </animated.div>
-      <animated.div style={animateHome} className="homePage">
+    <animated.div style={animateSplash} className="splash">
+      <LoadingSplash id="hideMe"/>
+    </animated.div>
+    <animated.div style={animateHome} className="homePage">
       <h1 className="title font-weight-bold text-center">Aarhus duck race</h1>
       <div className="video"></div>
       <div className="d-flex">
