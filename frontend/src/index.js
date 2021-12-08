76fd7cd62dff380762fecb5f44738082468f5239
Alex <alexmoller02@hotmail.com>
2021-12-08 10:35:16 +0100

    Fixede Router fejl

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,15 @@ import App from './App';
 import { Navigation as Navbar } from './Layout/Navbar';
 import { Sponsors as Sponsor } from './components/Sponsors/Sponsors';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Router>
     <Navbar />
     <App />
     <Sponsor />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
