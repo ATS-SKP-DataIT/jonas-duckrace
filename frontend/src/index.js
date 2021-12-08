079990bb0ed846c9b2a24ffccfb85429017eea05
Schou <Jonasschou@live.dk>
2021-12-08 10:34:58 +0100

    Basename ændring

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/aarhus-duck-race-demo">
+    <BrowserRouter basename="/skp-projekt-aarhus-duck-race">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
