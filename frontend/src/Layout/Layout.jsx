59d2527dc495c6fd5062b2a15d62051db43a00b5
Schou <Jonasschou@live.dk>
2021-12-08 10:47:26 +0100

    Navbar quickfix

--- frontend/src/Layout/Layout.jsx
+++ frontend/src/Layout/Layout.jsx
@@ -6,8 +6,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 export const Layout = (props) => (
   <div>
-    <Navigation/>
-    <Sponsors />
     <div className="container">{props.children}</div>
   </div>
 );
