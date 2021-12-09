c770d33bb7e963db8bfabd443df08b1d11164b4c
Alex <alexmoller02@hotmail.com>
2021-12-09 09:14:07 +0100

    New styling system

---
+++ frontend/src/Layout/style.js
@@ -0,0 +1,15 @@
+import { makeStyles, alpha } from '@material-ui/core/styles';
+
+const drawerWidth = 0;
+
+export default makeStyles((theme) => ({
+    appBar: {
+        boxShadow: "none"
+    },
+    toolBar: {
+        backgroundColor: "#EFA80A"
+    },
+    image: {
+        marginRight: "10px"
+    }
+}));
