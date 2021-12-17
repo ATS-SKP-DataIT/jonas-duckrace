a15ea1ccbbde5ce732020b8bfd32c2fe320af8bc
Alex <alexmoller02@hotmail.com>
2021-12-17 12:40:51 +0100

    Login fungere (uden sessions)

--- frontend/src/server/index.js
+++ frontend/src/server/index.js
@@ -3,11 +3,31 @@ const app = express();
 const cors = require("cors");
 const pool = require("./db")
 const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 //Middleware
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true
+}));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(session({
+    key: "userId",
+    secret: "skpdatait2021!",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 60 * 60 * 24
+    }
+}))
 
 
 //Create user
@@ -27,14 +47,38 @@ app.post("/create", async(req,res) => {
 })
 //Get user
 app.get("/login1", async(req,res) => {
-    try{
-        const results = await pool.query("SELECT * FROM accounts WHERE username = $1 AND password = $2 returning *",
-        [req.body.username, req.body.password])
-        console.log("success")
-    }catch(err){
-        console.log(err)
+    if(req.session.user){
+        res.send({loggedIn: true, user: req.session.user})
+    }else{
+        res.send({loggedIn: false})
     }
-    res.send("login")
+})
+
+//Check for account matching entered credentials on login page
+app.post("/login1", async(req,res) =>{
+    const username = req.body.username;
+    const password = req.body.password;
+    try{
+        const results = await pool.query("SELECT * FROM accounts WHERE username = $1;",
+        [username]) 
+            console.log(results.rows[0].password)
+        if (results){
+            console.log("Found username..")
+            bcrypt.compare(password, results.rows[0].password, (error, response) => {
+                if (response) {
+                    req.session.user = results.rows[0]
+                    console.log(req.session.user)
+                }else(
+                    console.log("Wrong password...")
+                )
+            })
+        }else{
+            console.log("User does not exist!")
+        }
+
+    }catch(err){
+        console.error(err.message)
+    }
 })
 
 app.listen(5000, () => {
